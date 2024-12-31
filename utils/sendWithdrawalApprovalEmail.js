3b9ec7ac395387a3c1113eb2119f7bbc3ae33470
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2024-12-31 12:58:23 +0100

    update

--- utils/sendWithdrawalApprovalEmail.js
+++ utils/sendWithdrawalApprovalEmail.js
@@ -18,6 +18,7 @@ const sendWithdrawalApprovalEmail = async (
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
@@ -82,6 +83,7 @@ const sendWithdrawalRejectionEmail = async (
   const transporter = nodemailer.createTransport({
     host: process.env.EMAIL_HOST,
     port: 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
